b90dde4feab6a11d968c6b413895c9b5a884ac9b
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-07 16:01:01 +0900

    [Bronze V] Title: A+B - 4, Time: 220 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            System.out.println(x+y);
+        }
+
+
+
+    }
+}
