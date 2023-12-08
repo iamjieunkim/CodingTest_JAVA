aca7d5c2e565b66d5e70bfb0daa697cc17959475
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-09 00:42:30 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        int c=0;
+        int n = sc.nextInt();
 
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        int a=sc.nextInt();
+        int v = sc.nextInt();
+        int cnt = 0;
 
-        for(int i=0;i<n;i++){
-            if(arr[i]==a){
-                c++;
+        for (int j=0; j<arr.length; j++){
+            if(arr[j]==v){
+                cnt++;
             }
         }
-        System.out.println(c);
 
+        System.out.println(cnt);
 
     }
+
 }
