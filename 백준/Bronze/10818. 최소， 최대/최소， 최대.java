bec7c49dd60b0aef16605268e704beb05fed69a2
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-09 00:35:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 1700 ms, Memory: 319256 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,20 +1,31 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-        
-		int N = in.nextInt();
-		int[] arr = new int[N];
-        
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
-		}
-        
-		in.close();
-		Arrays.sort(arr);
-		System.out.print(arr[0] + " " + arr[N - 1]);
-	}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }else if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.printf("%d %d", min, max);
+
+    }
+
 }
