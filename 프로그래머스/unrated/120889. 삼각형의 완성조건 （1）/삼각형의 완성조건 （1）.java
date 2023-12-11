693bd6369545f5d1add67d56bae8c8479837fd98
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-12 00:36:32 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.34 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,27 +1,10 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] sides) {
         int answer = 0;
         
-        int max=sides[0];
-        int min=sides[0];
-        int center = sides[0];
+        Arrays.sort(sides);
         
-        for(int i=0; i<sides.length; i++){
-            if(sides[i]>max){
-                max = sides[i];
-            }else if(sides[i]<min){
-                min = sides[i];
-            }else if(min<sides[i] && max>sides[i]){
-                center = sides[i];
-            }
-        }
-        
-        if(max<min+center){
-            answer=1;
-        }else{
-            answer=2;
-        }
-        
-        return answer;
+        return sides[2] < sides[0]+sides[1]? 1 : 2;
     }
 }
