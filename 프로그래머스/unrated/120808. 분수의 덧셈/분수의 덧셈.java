26079f929bdfc6503b77a3b3122fa09a7f281dd0
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-14 21:41:26 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         
+        int[] answer = new int[2];
+        
         int denom = denom1*denom2;
         int numer = numer1*denom2 + numer2*denom1;
         int cnt = 1;
@@ -11,7 +13,8 @@ class Solution {
             }
         }
         
-        int[] answer = {numer/cnt, denom/cnt};
+         answer[0] = numer/cnt;
+         answer[1] = denom/cnt;
         
         return answer;
         
