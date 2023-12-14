936a7e38c964a0e024edc3fc4c7e56c21cca7e0e
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-14 23:11:52 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int n) {
-        
-        int cnt = 0;
-        for(int i=1;i <= 6;i++){
-            if(n%i == 0 && 6%i == 0){ 
-                cnt = i;
-            }
+        int answer = 1;
+
+        while(true){
+            if(6 * answer % n == 0) break; 
+                answer++; 
         }
-        int answer = n/cnt;
-        
+ 
         return answer;
     }
 }
