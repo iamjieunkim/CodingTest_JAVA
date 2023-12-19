aae0ebe13588a1d490ce50d5509ffd7534248fbf
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-19 21:30:45 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        int idx = str1.indexOf(str2);
+        
+        
+        
+        return  answer = idx==(-1)?2:1;
+    }
+}
