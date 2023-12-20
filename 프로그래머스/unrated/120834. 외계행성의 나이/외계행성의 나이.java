1252a35e5806d95e78ee8c1d5e139ca7d0dd58f7
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2023-12-20 23:48:32 +0900

    [level unrated] Title: 외계행성의 나이, Time: 1.13 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,18 +1,13 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        String[] ans = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        String[] str = new String[]{"a","b","c","d","e","f","g","h","i","j"};
 
-        if(age==1000){
-            answer="baaa";
-        }else if(age>=100){
-            answer=ans[age/100]+ans[(age%100/10)]+ans[(age%100%10)];
-        }else if(age>=10){
-            answer=ans[age/10]+ans[age%10];
-        }else if(age>=0){
-            answer=ans[age];
+        while(age > 0){
+            answer = str[age % 10] + answer;
+            age /= 10;
         }
-        
+
         return answer;
     }
 }
