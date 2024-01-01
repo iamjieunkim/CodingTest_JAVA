dfffaad6379149884ca1e848a3abdaf8f3451d4c
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-01-02 01:45:18 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[1+(num_list.length-1)/n];
+        
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i]=num_list[i*n];
+        }
+        
+        return answer;
+    }
+}
