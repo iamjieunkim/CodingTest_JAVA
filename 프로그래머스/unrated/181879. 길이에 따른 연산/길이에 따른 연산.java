342c98e479b4ae43d2f97daf333601586d96066a
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-01-02 02:11:11 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int mul = 1;
+        int mult = 1;
         
-        if(num_list.length>=11){
-            for(int i=0; i<num_list.length; i++){
+        
+        for(int i=0; i<num_list.length; i++){
+            if(num_list.length>=11){
                 answer += num_list[i];
-            }
-        }else{
-            for(int i=0; i<num_list.length; i++){
-                mul *= num_list[i];
-                answer = mul;
+            }else{
+                mult *= num_list[i];
+                answer = mult;
             }
         }
+        
         return answer;
     }
 }
