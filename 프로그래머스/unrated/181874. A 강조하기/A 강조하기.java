8ff107ddfaae03383e6442acf74d8cf59580060b
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-01-04 00:03:15 +0900

    [level unrated] Title: A 강조하기, Time: 0.07 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer = (myString.toLowerCase()).replaceAll("a", "A");
+        
+        
+        return answer;
+    }
+}
