4a48a82054e991b2906465c0419f91107fd0f5b7
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-01-05 01:24:41 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -2,27 +2,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[9];
+        int arr[] = new int[9];
 
-        for(int i=0; i<arr.length; i++) {
+        for (int i=0; i<9; i++){
             arr[i] = sc.nextInt();
         }
 
         int max = arr[0];
-        int cnt =0;
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i]>max){
-                max=arr[i];
-                cnt=i;
+        int idx = 0;
+
+        for (int i=0; i<9; i++){
+            if(max<arr[i]){
+                max = arr[i];
+                idx = i;
             }
         }
 
         System.out.println(max);
-        System.out.println(cnt+1);
-    }
+        System.out.print(idx+1);
 
+    }
 }
