a2dc41df0ed9ebfa4cdfb564ab6bf7d17d8addd8
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-01-09 22:53:36 +0900

    [level unrated] Title: 꼬리 문자열, Time: 1.61 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String ans = "";
+        
+         for (int i=0; i<str_list.length; i++){
+            if(str_list[i].indexOf(ex)==-1){
+               ans += str_list[i];
+            }
+        }
+        
+        return ans;
+    }
+}
