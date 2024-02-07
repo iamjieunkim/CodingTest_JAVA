2794ec8f05309ab21627efa53e0c75af1d77f70a
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-02-07 23:29:40 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String[] arr = new String[array.length];
+
+        for(int i=0; i< array.length; i++){
+            arr[i] = Integer.toString(array[i]);
+        }
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length(); j++) {
+                if (arr[i].charAt(j) == '7') {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
