fbac167aa1fc80adcba4d1d020d80ebb9da16dec
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-03-12 02:04:54 +0900

    [D1] Title: 대각선 출력하기, Time: 100 ms, Memory: 19,224 KB -BaekjoonHub

--- SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -36,10 +36,15 @@ class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-		System.out.println("#++++");
-        System.out.println("+#+++");
-        System.out.println("++#++");
-        System.out.println("+++#+");
-        System.out.println("++++#");
+		for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if(i==j){
+                    System.out.print("#");
+                }else{
+                    System.out.print("+");
+                }
+            }
+            System.out.println();
+        }
 	}
 }
