d4c70c20a08194b5d38ae4c4370b398666b309ff
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-03-19 23:21:27 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -3,8 +3,8 @@ class Solution {
         int[] answer = new int[numbers.length];
 
         if(direction.equals("right")){
+            answer[0] = numbers[numbers.length-1];
             for(int i=0; i< numbers.length-1; i++){
-                answer[0] = numbers[numbers.length-1];
                 answer[i+1] = numbers[i];
             }
         }else if(direction.equals("left")){
