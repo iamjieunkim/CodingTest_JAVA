da7fc4776960e8dc2f83b84017292c6b3315aa07
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-03-19 23:59:51 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0; 
+
+        for (int i = 4; i <= n; i++) { 
+            int idx = 0; 
+            for (int j = 1; j <= i; j++) { 
+                if (i % j == 0) { 
+                    idx++; 
+                }
+            }
+            if (idx >= 3) { 
+                answer++; 
+            }
+        }
+        return answer;
+    }
+}
