714e7ec3107b344d2cd609198160650469943ac0
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-04-13 02:28:15 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.33 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -13,18 +13,18 @@ class Solution {
         
         if(dice[0]==dice[3]){
             answer = 1111*a;
-        }else if(dice[0]==dice[2]&&dice[0]!=dice[3]){
+        }else if(dice[0]==dice[2]){
             answer = (10*dice[0]+dice[3])*(10*dice[0]+dice[3]);
-        }else if(dice[1]==dice[3]&&dice[0]!=dice[3]){
+        }else if(dice[1]==dice[3]){
             answer = (10*dice[3]+dice[0])*(10*dice[3]+dice[0]);
-        }else if(dice[0]==dice[1]&&dice[2]==dice[3]&&dice[0]!=dice[3]){
+        }else if(dice[0]==dice[1]&&dice[2]==dice[3]){
             answer = (dice[0]+dice[2]) * Math.abs(dice[0]-dice[2]);
-        }else if(dice[0]==dice[1]&&dice[0]!=dice[2]&&dice[0]!=dice[3]){
+        }else if(dice[0]==dice[1]){
             answer = dice[2]*dice[3];
-        }else if(dice[1]==dice[2]&&dice[1]!=dice[0]&&dice[1]!=dice[3]){
+        }else if(dice[1]==dice[2]){
             answer = dice[0]*dice[3];
         }
-        else if(dice[2]==dice[3]&&dice[2]!=dice[0]&&dice[2]!=dice[1]){
+        else if(dice[2]==dice[3]){
             answer = dice[0]*dice[1];
         }else{
             answer = dice[0];
