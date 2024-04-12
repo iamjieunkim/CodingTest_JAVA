9cb728cc4503bb4e0ddaa9da077896ecde1c1520
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-04-13 02:22:25 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.32 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] dice = new int[4];
+        int answer = 0;
+        dice[0] = a;
+        dice[1] = b;
+        dice[2] = c;
+        dice[3] = d;
+        
+        
+        Arrays.sort(dice);
+        
+        if(dice[0]==dice[3]){
+            answer = 1111*a;
+        }else if(dice[0]==dice[2]&&dice[0]!=dice[3]){
+            answer = (10*dice[0]+dice[3])*(10*dice[0]+dice[3]);
+        }else if(dice[1]==dice[3]&&dice[0]!=dice[3]){
+            answer = (10*dice[3]+dice[0])*(10*dice[3]+dice[0]);
+        }else if(dice[0]==dice[1]&&dice[2]==dice[3]&&dice[0]!=dice[3]){
+            answer = (dice[0]+dice[2]) * Math.abs(dice[0]-dice[2]);
+        }else if(dice[0]==dice[1]&&dice[0]!=dice[2]&&dice[0]!=dice[3]){
+            answer = dice[2]*dice[3];
+        }else if(dice[1]==dice[2]&&dice[1]!=dice[0]&&dice[1]!=dice[3]){
+            answer = dice[0]*dice[3];
+        }
+        else if(dice[2]==dice[3]&&dice[2]!=dice[0]&&dice[2]!=dice[1]){
+            answer = dice[0]*dice[1];
+        }else{
+            answer = dice[0];
+        }
+        
+        
+        return answer;
+    }
+}
