d63bf6fa590f9a89022887bcc88d6fa73879fe19
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-04-18 00:43:09 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution {
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.print(T.solution(num1, num2));
+    }
+}
