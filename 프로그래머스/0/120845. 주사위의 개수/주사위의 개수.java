15039943ba4ab5817e6aefa7a33a58f254a11ecb
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-04-23 00:14:43 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Solution {
     public int solution(int[] box, int n) {
         int answer = 1;
@@ -9,5 +10,18 @@ class Solution {
         answer = a*b*c;
         
         return answer;
+    }
+    
+    public static void main(String[] args){
+        Solution T = new Solution();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        int[] box = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            box[i] = Integer.parseInt(arr[i]);
+        }
+        int n = sc.nextInt();
+        System.out.println(T.solution(box, n));
     }
 }
