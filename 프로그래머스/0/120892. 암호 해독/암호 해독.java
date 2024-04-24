1661e363a2fb872ae67b9c38961ba40dabdcd882
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-04-25 00:07:29 +0900

    [level 0] Title: 암호 해독, Time: 9.57 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
@@ -7,5 +8,12 @@ class Solution {
         }
         
         return answer;
+    }
+    public static void main(String[] args){
+        Solution T = new Solution();
+        Scanner sc = new Scanner(System.in);
+        String cipher = sc.next();
+        int code = sc.nextInt();
+        System.out.println(T.solution(cipher, code));
     }
 }
