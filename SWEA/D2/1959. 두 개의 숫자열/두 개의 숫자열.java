b4c9611d460ce7a2b5780fd851cac83f5c49de32
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-05-14 22:29:59 +0900

    [D2] Title: 두 개의 숫자열, Time: 149 ms, Memory: 26,444 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -9,43 +9,47 @@ class Solution
         int T;
         T=sc.nextInt();
          
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
+        for(int tc=1; tc<=T; tc++){
             int N = sc.nextInt();
             int M = sc.nextInt();
-             
+
             int[] A = new int[N];
+            for(int i=0; i<N; i++){
+                A[i]=sc.nextInt();
+            }
             int[] B = new int[M];
-             
-            for(int i = 0; i < N; i++) A[i] = sc.nextInt();
-            for(int i = 0; i < M; i++) B[i] = sc.nextInt();
-             
+            for(int j=0; j<M; j++){
+                B[j]=sc.nextInt();
+            }
+
             int max = 0;
-             
-            if(N < M) {
-                for(int i = 0; i < M-N+1; i++) {
+            if(N<M){
+                for(int i=0; i<M-N+1; i++){
                     int result = 0;
-                    for(int j = 0; j < N; j++) {
+                    for(int j=0; j<N; j++){
                         result += A[j]*B[i+j];
                     }
                     max = Math.max(max, result);
                 }
-            } else if (N > M) {
-                for(int i = 0; i < N-M+1; i++) {
-                    int result = 0;
-                    for(int j = 0; j < M; j++) {
+            } else if (M<N) {
+                for(int i=0; i<N-M+1; i++){
+                    int result=0;
+                    for(int j=0; j<M; j++){
                         result += A[i+j]*B[j];
                     }
                     max = Math.max(max, result);
                 }
             } else {
-                int result = 0;
-                for(int i = 0; i < N; i++) {
-                    result += A[i]*B[i];
+                for(int i=0; i<N; i++){
+                    int result = 0;
+                    for(int j=0; j<M; j++){
+                        result += A[i]*B[j];
+                    }
+                    max = Math.max(max, result);
                 }
-                max = Math.max(max, result);
             }
-            System.out.printf("#%d %d\n", test_case, max);
+
+            System.out.println("#"+tc+" "+max);
         }
     }
 }
