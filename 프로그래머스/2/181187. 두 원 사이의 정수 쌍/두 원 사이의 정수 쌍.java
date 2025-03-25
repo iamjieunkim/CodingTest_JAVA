3f1db7ca26fe46faa2701bece4361afc6e5c9fbd
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2025-03-25 14:12:03 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 24.09 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        
+        for(int i=1; i<=r2; i++){
+            long min = (int)Math.ceil(Math.sqrt(1.0*r1*r1-1.0*i*i));
+            long max = (int)Math.floor(Math.sqrt(1.0*r2*r2-1.0*i*i));
+            
+            answer += (max-min+1);
+            
+        }
+        
+        answer*=4;
+        
+        return answer;
+    }
+    
+    
+}
